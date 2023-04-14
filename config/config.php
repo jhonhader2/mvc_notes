06a3f9f25eb2758498cae2d108597cf53d399738
Jhon Hader Rodriguez <jhonhader2@hotmail.com>
2023-04-14 01:16:19 -0500

    Commint_init

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "mvc_notes");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+/* Default options */
+define("DEFAULT_CONTROLLER", "note");
+define("DEFAULT_ACTION", "list");
---
+++ view/delete_note.php
@@ -0,0 +1,5 @@
+<div class="row">
+	<div class="alert alert-success">
+		Nota eliminada correctamente. <a class="btn btn-sm btn-primary" href="index.php?controller=note&action=list">Volver al listado</a>
+	</div>
+</div>
